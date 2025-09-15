522168a075af485c70671d84054a9bab4121825b
JayYosh <133554802+JayYosh@users.noreply.github.com>
2025-09-14 21:18:05 -0500

    add docker file. add basic controller. added frontend boilerplate

---
+++ backend/src/main/java/app/backend/HomeController.java
@@ -0,0 +1,13 @@
+package app.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;;
+
+@RestController
+public class HomeController{
+
+    @GetMapping("/")
+    public String index(){
+        return "Start up success";
+    }
+}
---
+++ frontEnd/onlineMastermind--/src/components/menu/playButton.tsx
@@ -0,0 +1,13 @@
+const PlayButton = () => {
+
+
+  const handleHomeButtonClick = () => {
+
+  }
+
+  return (
+    <button className="PlayButton" onClick={handleHomeButtonClick}>Add 1</button>
+  )
+}
+
+export default PlayButton;
